6b694d1ab7b6f6f54d7566638840b3f7154f287d
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-11-06 19:18:21 +0200

    For loop for test cases
    Added a for loop to loop over the number of the test cases within the given input

--- main.cpp
+++ main.cpp
@@ -13,8 +13,11 @@ struct Item {
 void readInputFromFile(const string& filename, int& numTestCases, vector<vector<Item>>& testCases) {
     ifstream input(filename);
     if (!input.is_open()) {
-        cerr << "Error: Unable to open input file." << endl;
+        cerr << "Error: Unable to open input file" << endl;
         exit(1);
+    }
+    else {
+        cout << "file openned successfully" << endl;
     }
 
     input >> numTestCases;
@@ -38,7 +41,16 @@ int main() {
     vector<vector<Item>> testCases;
     readInputFromFile("input.txt", numTestCases, testCases);
 
-    
+    for (int testCaseIndex = 0; testCaseIndex < numTestCases; ++testCaseIndex) {
+        
+        int knapsackSize = testCases[testCaseIndex][0].weight;
+        vector<Item>& items = testCases[testCaseIndex];
+        
+        
+        
+        cout << "Test Case " << testCaseIndex + 1 << ":\n";
+    }
+    cout << "Number of test cases: " << numTestCases << endl;
 
     return 0;
 }
