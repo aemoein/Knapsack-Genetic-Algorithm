5af450e679da341a36db88eb4252d8e4debb36d0
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-11-06 18:11:58 +0200

    ReadInput Function
    added a function to read input from the file and add it to vector and variables

--- main.cpp
+++ main.cpp
@@ -10,6 +10,29 @@ struct Item {
     int value;
 };
 
+void readInputFromFile(const string& filename, int& numTestCases, vector<vector<Item>>& testCases) {
+    ifstream input(filename);
+    if (!input.is_open()) {
+        cerr << "Error: Unable to open input file." << endl;
+        exit(1);
+    }
+
+    input >> numTestCases;
+    testCases.resize(numTestCases);
+
+    for (int i = 0; i < numTestCases; ++i) {
+        int knapsackSize, numItems;
+        input >> knapsackSize >> numItems;
+        testCases[i].resize(numItems);
+
+        for (int j = 0; j < numItems; ++j) {
+            input >> testCases[i][j].weight >> testCases[i][j].value;
+        }
+    }
+
+    input.close();
+}
+
 int main() {
 
     return 0;
